de7f81f8f942b8aa295de4640136e9a0ace2309a
Christopher Wong <cwong8257@gmail.com>
2018-02-26 07:54:01 -0500

    Fetch firebase expenses on start

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -5,9 +5,4 @@ import Header from '../../components/Header';
 test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
   expect(wrapper).toMatchSnapshot();
-
-  // expect(wrapper.find('h1').length).toBe(1);
-  // const renderer = new ReactShallowRenderer();
-  // renderer.render(<Header />);
-  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
